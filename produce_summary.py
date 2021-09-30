ee2ef781524aabe4799e90e0401d8f5d9405f98b
Courtney Doss <777.catalyst@gmail.com>
2021-09-30 16:29:41 -0500

    Create daily report function

---
+++ produce_summary.py
@@ -0,0 +1,58 @@
+def print_daily_report(day_num, file_name):
+    print(f"Day {day_num}")
+    the_file = open(file_name)
+    for line in the_file:
+        line = line.rstrip()
+        words = line.split('|')
+
+        melon = words[0]
+        count = words[1]
+        amount = words[2]
+
+        print(f"Delivered {count} {melon}s for total of ${amount}")
+    the_file.close()
+
+
+print_daily_report(1, "um-deliveries-20140519.txt")
+
+
+# print("Day 1")
+# the_file = open("um-deliveries-20140519.txt")
+# for line in the_file:
+#     line = line.rstrip()
+#     words = line.split('|')
+
+#     melon = words[0]
+#     count = words[1]
+#     amount = words[2]
+
+#     print(f"Delivered {count} {melon}s for total of ${amount}")
+# the_file.close()
+
+
+# print("Day 2")
+# the_file = open("um-deliveries-20140520.txt")
+# for line in the_file:
+#     line = line.rstrip()
+#     words = line.split('|')
+
+#     melon = words[0]
+#     count = words[1]
+#     amount = words[2]
+
+#     print(f"Delivered {count} {melon}s for total of ${amount}")
+# the_file.close()
+
+
+# print("Day 3")
+# the_file = open("um-deliveries-20140521.txt")
+# for line in the_file:
+#     line = line.rstrip()
+#     words = line.split('|')
+
+#     melon = words[0]
+#     count = words[1]
+#     amount = words[2]
+
+#     print(f"Delivered {count} {melon}s for total of ${amount}")
+# the_file.close()
